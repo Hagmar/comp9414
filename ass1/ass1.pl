ea2ca14a8fccd5b08efec50bdd5a485548c5dabe
Magnus Alexander Hagmar <m.hagmar@student.unsw.edu.au>
2016-03-18 13:10:54 +1100

    Changed several solutions to not use the cut goal. Also added comments

--- ass1/ass1.pl
+++ ass1/ass1.pl
@@ -3,15 +3,21 @@
 % Assignment 1 - Prolog Programming
 
 
-% TODO Solve without cut?
-% Sums the squares of only even numbers in a list of numbers
+% sumsq_even(+Numbers, -Sum)
+% Binds Sum to the sum of all even numbers in the list.
+% Numbers must be a list only containing integers.
+%
+% Assignment comment:
+% I would have prefered to include a cut after the check whether Head is even
+% or not, as this would remove the need for confirming that it is odd in the
+% other case.
 sumsq_even([], 0).
 sumsq_even([Head|Tail], Sum) :-
     0 is Head mod 2,
-    !,
     sumsq_even(Tail, RestSum),
     Sum is Head*Head + RestSum.
-sumsq_even([_|Tail], Sum) :-
+sumsq_even([Head|Tail], Sum) :-
+    1 is Head mod 2,
     sumsq_even(Tail, Sum).
 
 
@@ -25,35 +31,60 @@ likes(tim, apple).
 likes(jane, apple).
 likes(jane, mango).
 
-% TODO Add comments
+
+% all_like(+What, +List)
+% Succeeds if every person in the list List likes the item What.
+% "Liking" an item is represented by the predicate likes(Person, Item).
 all_like(_, []).
 all_like(What, [Person|Tail]) :-
     likes(Person, What),
     all_like(What, Tail).
 
 
-% TODO Add comments
-% TODO Causes an error after finding solution
+% sqrt_table(+N, +M, -Result)
+% Binds Result to a list containing elements of the form [X, Y] where Y is
+% the square root of X. The Xs in the list start at N and decrease by one
+% until they reach M.
+% M is required to be less than or equal to N.
+%
+% Assignment comment:
+% Again, I would prefer to use a cut as the first goal when N and M are the
+% same, as this would prevent unnecessary backtracking and remove the need
+% for the N =\= M check.
 sqrt_table(N, N, [[N, Result]]) :-
     Result is sqrt(N).
 sqrt_table(N, M, [[N, NResult] | Result]) :-
+    N =\= M,
     NResult is sqrt(N),
     NewN is N-1,
     sqrt_table(NewN, M, Result).
 
 
-% TODO Causes an error after finding solution
+% function_table(+N, +M, +Function, -Result)
+% Binds Result to a list containing elements of the form [X, Y] where Y is
+% Function(X). The Xs in the list start at N and decrease by one until they
+% Reach M.
+% M is required to be less than or equal to N.
+%
+% Assignment comment:
+% I am not in COMP9814 but wanted to attempt the question anyway.
+% A cut in the N, N case would be better than the N=\=M check in the other.
 function_table(N, N, Function, [[N, Result]]) :-
     Temp =.. [Function, N],
     Result is Temp.
 function_table(N, M, Function, [[N, Result]|RestResult]) :-
+    N =\= M,
     Temp =.. [Function, N],
     Result is Temp,
     NewN is N-1,
     function_table(NewN, M, Function, RestResult).
 
 
-% TODO Add comments
+% chop_down(+List, -NewList)
+% Binds NewList to List with all sequences of successive decreasing numbers
+% removed.
+% List should only contain integers.
+% TODO Solve without cuts
 chop_down([], []).
 chop_down([N | [M | Tail]], Result) :-
     M is N-1,
@@ -63,7 +94,17 @@ chop_down([N| Tail], [N | Result]) :-
     chop_down(Tail, Result).
 
 
-% TODO Add comments
+% tree_eval(+Value, +Tree, -Eval)
+% Takes a tree representing a mathematical formula, possibly containing
+% variables, assigns the value Value to the variables, and evaluates the
+% tree mathematically.
+% Value must be a numerical value
+% Tree needs to be a valid tree in one of the following forms:
+%   - tree(empty, Num, empty), where Num is a number
+%   - tree(empty, z, empty), which is a variable
+%   - tree(L, Op, R), where L and R are other trees, and Op is an arithmetic
+% operator
+%
 % TODO Solve without cuts
 tree_eval(Value, tree(empty, z, empty), Value) :-
     !.
